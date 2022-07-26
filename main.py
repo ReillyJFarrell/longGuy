0a834ff0fb312eb12d8029fbd73dd4dd423d0a18
reilly <reillyjfarrell@gmail.com>
2022-07-26 02:45:42 -0500

    lightbulb fix13

--- main.py
+++ main.py
@@ -24,12 +24,13 @@ if __name__ == '__main__':
     bot = lightbulb.BotApp(token='MTAwMTI0MTE3MTY4MDU2NzQ0OA.GFurCp.jbufJUvJDP3BHkZ6fHp_dYV6dPZ3SdZn3lw5JY'
                            , default_enabled_guilds=(355444520378302464, 318564596321615892)
                            )
+    items = getItems()
     @bot.command
     @lightbulb.command('fg-store', 'Gives current Fall Guys Shop Items')
     @lightbulb.implements(lightbulb.SlashCommand)
     async def fgstore(ctx):
         # await ctx.respond('Current Items:')
-        for ii in getItems():
+        for ii in items:
            await ctx.respond(ii)
 
     bot.run()
