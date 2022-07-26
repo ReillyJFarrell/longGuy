c7af049f63c08b70194fa78cbe02113e2265a936
ReillyJFarrell <66333267+ReillyJFarrell@users.noreply.github.com>
2022-07-26 01:33:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -36,13 +36,13 @@ if __name__ == '__main__':
     bot = lightbulb.BotApp(token='MTAwMTI0MTE3MTY4MDU2NzQ0OA.GFurCp.jbufJUvJDP3BHkZ6fHp_dYV6dPZ3SdZn3lw5JY'
                            , default_enabled_guilds=(355444520378302464, 318564596321615892)
                            )
-    @bot.command
-    @lightbulb.command('fg-store', 'Gives current Fall Guys Shop Items')
-    @lightbulb.implements(lightbulb.SlashCommand)
-    async def ping(ctx):
-        await ctx.respond('Current Items:')
-        for ii in getItems():
-            await ctx.respond(ii)
+   # @bot.command
+    #@lightbulb.command('fg-store', 'Gives current Fall Guys Shop Items')
+    #@lightbulb.implements(lightbulb.SlashCommand)
+    #async def ping(ctx):
+     #   await ctx.respond('Current Items:')
+      #  for ii in getItems():
+       #     await ctx.respond(ii)
 
     bot.run()
 
