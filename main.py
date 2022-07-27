2b03ae65b50e4d2ad1b53db204b1db686bf3e2c1
reilly <reillyjfarrell@gmail.com>
2022-07-27 01:36:44 -0500

    selenium driver chrome path update2

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import time
 def scrape_store():
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("$GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
